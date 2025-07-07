ec351c4a9b12bd4b6da7174b2c3aa85137403fef
Jonathan Goh <jonathangohmh@gmail.com>
2025-07-07 23:04:06 +0800

    Initial Setup for Flask and React

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='flex justify-center items-center h-screen border-4'>
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
