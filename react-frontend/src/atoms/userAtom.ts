a92be198f412e2f7d7f8c0079b6976e689b35dd5
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-07-09 12:01:02 +0800

    Initialized userAtom

--- react-frontend/src/atoms/userAtom.ts
+++ react-frontend/src/atoms/userAtom.ts
@@ -1,2 +1,4 @@
 import { atomWithStorage } from "jotai/utils";
 import type { User } from "../types/user";
+// Initialize userAtom to a null state.
+export const userAtom = atomWithStorage<User | null>('user', null);
